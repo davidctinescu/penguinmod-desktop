cae84379cbd107005463a5269d1799bb8af4bc75
david <david.andrei.pisoi@gmail.com>
2024-05-22 20:40:44 +0300

    first commit

---
+++ src/main.js
@@ -0,0 +1,48 @@
+const { app, BrowserWindow, globalShortcut } = require('electron');
+const path = require('path');
+
+let mainWindow;
+
+function createWindow() {
+    mainWindow = new BrowserWindow({
+        width: 800,
+        height: 600,
+        webPreferences: {
+            nodeIntegration: true
+        },
+        autoHideMenuBar: true,
+        icon: path.join(__dirname, '../app/images/512.png')
+    });
+
+    mainWindow.loadFile(path.join(__dirname, '../app/editor.html'));
+
+    mainWindow.on('closed', () => {
+        mainWindow = null;
+    });
+
+    mainWindow.maximize();
+}
+
+app.whenReady().then(() => {
+    createWindow();
+
+    globalShortcut.register('Control+Q', () => {
+        app.quit();
+    });
+});
+
+app.on('window-all-closed', () => {
+    if (process.platform !== 'darwin') {
+        app.quit();
+    }
+});
+
+app.on('activate', () => {
+    if (mainWindow === null) {
+        createWindow();
+    }
+});
+
+app.on('will-quit', () => {
+    globalShortcut.unregisterAll();
+});
